825449739ef2d1dc79a697f6f2571f01854c5e94
Nicholas Angeli <nicangeli@gmail.com>
2013-05-14 17:28:15 +0100

    Builds html details for one URL into array objects

--- index.js
+++ index.js
@@ -3,8 +3,49 @@ var $ 		= 	require('cheerio'),
 
 // Games start from id 457 and end at 492
 
-var url = "http://tac.ecs.soton.ac.uk:8080/history/";
+var url = "http://tac.ecs.soton.ac.uk:8080/history/457/",
+	agentNames = [];
 
-for(var i = 457; i < 493; i++) {
-	console.log(url + i + '/');
-}
+request(url, function(err, res, html) {
+	if(err)
+		throw err;
+
+	var transactionNames = [],
+		transactionDay = [],
+		transactionBuyer = [],
+		transactionQuantity = [],
+		transactionPrice = [];
+
+	var parsedHtml = $.load(html);
+	parsedHtml('tr :nth-child(2)').each(function(i, row) {
+		if(i > 9)
+			transactionNames.push($(row).text());
+	});
+
+	parsedHtml('tr :nth-child(3)').each(function(i, row) {
+		if(i > 9)
+			transactionDay.push($(row).text());
+	});
+
+	parsedHtml('tr :nth-child(4)').each(function(i, row) {
+		if(i > 9)
+			transactionBuyer.push($(row).text());
+	});
+
+	parsedHtml('tr :nth-child(6)').each(function(i, row) {
+		if(i > 0)
+			transactionQuantity.push($(row).text());
+	});
+
+	parsedHtml('tr :nth-child(7)').each(function(i, row) {
+		if (i > 0)
+			transactionPrice.push($(row).text());
+	});
+
+	// got all of the details from the html page url now into arrays for name, day etc
+
+	// now lets just get the hotels as that's all i care about
+	transactionNames.each(function(index, element) {
+		console.log(element);
+	})
+});

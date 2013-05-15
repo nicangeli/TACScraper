30bbf9427ac0b081b95b790f2d183b10e8a046b4
Nicholas Angeli <nicangeli@gmail.com>
2013-05-15 11:18:31 +0100

    Gets average and myaverage for all games you participated in now

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ var hotel = require('./hotel_prices');
 
 
 var h = new hotel();
-/*h.average(457, function(overall, myagent) {
-	console.log('Overall hotel price: ' + overall);
-	console.log(process.argv[2] + ' hotel price: ' + myagent);
-})
-*/
-h.getGames('TacOBell', function(myGames) {
-	console.log(myGames);
+h.getGames('TacOBell', function(game_number) {
+	h.average(game_number, function(overall, myAverage) {
+		console.log(game_number + ' Overall Average: ' + overall);
+		console.log(game_number + ' ' + process.argv[2] + ' Average: ' + myAverage);
+	});
 });

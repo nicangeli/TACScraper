151249d65cd3c12a2bf291730c53c63e1f9273d3
Nicholas Angeli <nicangeli@gmail.com>
2013-05-15 11:14:53 +0100

    Now works out which game AgentName participated in

--- hotel_prices.js
+++ hotel_prices.js
@@ -1,10 +1,12 @@
 var $ 		= 	require('cheerio'),
-	request =	require('request');
+	request =	require('request'),
+	_ 		= 	require('underscore');
 
 module.exports = function() {
 
 	this.average = function(game_number, callback) {
 		var baseUrl = "http://tac.ecs.soton.ac.uk:8080/history/";
+		
 		request(baseUrl + game_number + '/', function(err, res, html) {
 			if(err)
 				throw err;
@@ -16,28 +18,30 @@ module.exports = function() {
 				transactionPrice = [],
 				transactions = [];
 
-			var parsedHtml = $.load(html);
-			parsedHtml('tr :nth-child(2)').each(function(i, row) {
+
+			var parsedHtml = $.load(html); // put html into cheerio
+
+			parsedHtml('tr :nth-child(2)').each(function(i, row) { // the auction name 
 				if(i > 9)
 					transactionNames.push($(row).text());
 			});
 
-			parsedHtml('tr :nth-child(3)').each(function(i, row) {
+			parsedHtml('tr :nth-child(3)').each(function(i, row) { // auction day
 				if(i > 9)
 					transactionDay.push($(row).text());
 			});
 
-			parsedHtml('tr :nth-child(4)').each(function(i, row) {
+			parsedHtml('tr :nth-child(4)').each(function(i, row) { // auction buyer
 				if(i > 9)
 					transactionBuyer.push($(row).text());
 			});
 
-			parsedHtml('tr :nth-child(6)').each(function(i, row) {
+			parsedHtml('tr :nth-child(6)').each(function(i, row) { // purchased quantity
 				if(i > 0)
 					transactionQuantity.push($(row).text());
 			});
 
-			parsedHtml('tr :nth-child(7)').each(function(i, row) {
+			parsedHtml('tr :nth-child(7)').each(function(i, row) { // single purchase price
 				if (i > 0)
 					transactionPrice.push($(row).text());
 			});
@@ -74,6 +78,25 @@ module.exports = function() {
 			});
 
 			callback(total/totalQuantity, myTotal/myQuantity);
+		});
+	}
+
+	this.getGames = function(agent_name, callback) {
+
+		var baseUrl = "http://tac.ecs.soton.ac.uk:8080/history/";
+		_.range(457, 493).forEach(function(game_number) {
+			request(baseUrl + game_number + '/', function(err, res, html) {
+				if (err)
+					throw err;
+				var parsedHtml = $.load(html);
+				parsedHtml('td:nth-child(1)').each(function(index, element) {
+					if(index < 8) {
+						if($(element).text() === agent_name) {
+							callback(game_number);
+						}
+					}
+				})
+			})
 		});
 	}
 
--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var hotel = require('./hotel_prices');
 
 
 
-// Games start from id 457 and end at 492
+
 
 var h = new hotel();
-h.average(457, function(overall, myagent) {
+/*h.average(457, function(overall, myagent) {
 	console.log('Overall hotel price: ' + overall);
 	console.log(process.argv[2] + ' hotel price: ' + myagent);
 })
+*/
+h.getGames('TacOBell', function(myGames) {
+	console.log(myGames);
+});
